d4b8ca31bf55f5f2d8d7cab1591e1e0b87744d99
div110 <davidxhalamka@gmail.com>
2024-09-16 21:07:39 +0200

    update format()

--- brdCal.c
+++ brdCal.c
@@ -2,9 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
 int format(char argv[], int order){
+int pos[5];
+pos[0]=0;
+
+int slot=1;
 char triple [3];
-for(int i =0; i<3; i++){
+
+
+
+for(int i=0; i<strlen(argv);i++){
+if(argv[i]=='.'){pos[slot]=i;slot = slot+1;
+	//printf("%d\n", i);
+}
+
+else{}
+}
+pos[4]=strlen(argv);
+
+//printf("%d%d%d\n", pos[0],pos[1],pos[2]);
+
+for(int i =pos[order-1]; i<pos[order]; i++){
+
+
 if(argv[i] == '.'){break;}
 else{triple[i] = argv[i];}
 }
@@ -15,17 +38,16 @@ return atoi(triple);}
 
 
 
+
+
+
+
 int main(int argc, char * argv[]){
 
-
-printf("%s\n%d\n", argv[1] , format(argv[1], 1));
-
-
+printf("%d\n", format(argv[1],1));
+printf("%d\n", format(argv[1],32));
+printf("%d\n", format(argv[1],3));
+printf("%d\n", format(argv[1],4));
 
 return 0;
-
-
 }
-
-
-
